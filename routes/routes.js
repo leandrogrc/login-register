59fb410cc7560212b29d407bcec139e76e4badc8
Leandro <leogarcia1997@gmail.com>
2024-06-27 10:08:36 -0300

    added static

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,6 @@ import { cadastrar, login } from '../controllers/controllers.js'
 
 const router = express.Router()
 
-//router.get('/')
-
 router.post('/cadastrar', cadastrar)
 
 router.post('/login', login)
